80287e5da604402e66b0713df0c912a23efb059f
kujtimpllana <kp44415@ubt-uni.net>
2023-05-25 11:02:25 +0200

    defined outlet for routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,11 @@
 import Home from "./pages/Home"
+import { Outlet } from "react-router-dom"
 
 function App() {
   
   return (
     <div className='app'>
-      <Home />
+      <Outlet />
     </div>
   )
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -9,15 +9,15 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import Home from "./pages/Home.jsx";
+import About from "./pages/About.jsx";
+import Contact from "./pages/Contact.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
-      <Route path="/" index element={<Home />} />
-      {/*
-      <Route path='/about' index element={ <About/> }/>
-      <Route path='/contact' index element={ <Home/> }/>
-      */}
+      <Route path="/" index exact element={<Home />} />
+      <Route path='/about' element={ <About/> }/>
+      <Route path='/contact' element={ <Contact/> }/> 
     </Route>
   )
 );
