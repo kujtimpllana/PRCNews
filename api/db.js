f25799611990daa1694a1ce5419fd5d1c1ef56ac
kujtimpllana <kp44415@ubt-uni.net>
2023-11-22 18:14:52 +0100

    credentials update

--- api/db.js
+++ api/db.js
@@ -3,6 +3,6 @@ import mysql from "mysql2";
 export const db = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "admin",
+  password: "toor",
   database: "prc_news",
 });
