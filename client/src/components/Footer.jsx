a04e6ed29ff7820579e9ff3505688c3ee32f7c4b
kujtimpllana <kp44415@ubt-uni.net>
2024-02-04 19:13:35 +0100

    small typo

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <p className="font-poppins font-semibold h-14 bg-[#000711] text-slate-100 flex justify-center items-center mt-auto w-full">
       <span className="font-poppins text-[#D0A650]">
-        Copyright &copy;{" "}
+        PRC News &copy;{" "}
         {date.getFullYear() === 2023 ? "2023" : "2023-" + date.getFullYear()}.
       </span>
       &nbsp; All rights reserved.
